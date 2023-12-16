930dd263b8348e6e49e97ff7790fe9e35001fb8f
Rafael Nunes <rafaelnunes@GMRC4493.local>
2023-12-15 21:09:41 -0300

    teste

--- app/components/Card/index.tsx
+++ app/components/Card/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 // import { Container } from './styles';
 interface CardProps {
-    texto: string
+    texto: string // posso colocar ? para não ser obrigatoria a variável "texto"
 }
-const Card: React.FC <CardProps> = ({texto} :CardProps) => {
-  return <h1>{texto}</h1>;
+const Card: React.FC <CardProps> = ({texto} :CardProps) => { // dois pontos significa TIPO e o => é o arrow function
+  return <h1>{texto}</h1>; // component + Props é o que o componente vai precisar para funcionar
 }
 
 export default Card;
